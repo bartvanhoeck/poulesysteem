a5fd8390540ef938ef6971a50e987d00f8347382
peterpadberg <peterpadberg@8b07a520-9580-ddfd-33fd-e530a354530b>
2010-12-02 11:11:21 +0000

    Poules na poule D geen null meer

--- trunk/Poulegenerator.java
+++ trunk/Poulegenerator.java
@@ -34,6 +34,12 @@ public class Poulegenerator{
             case 2: return "B";
             case 3: return "C";
             case 4: return "D";
+            case 5: return "E";
+            case 6: return "F";
+            case 7: return "G";
+            case 8: return "H";
+            case 9: return "I";
+            case 10: return "J";
         }
         return null;
     }
