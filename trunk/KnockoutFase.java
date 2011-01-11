e4aac59d576db1b78a8329cfc467cee9bf9e46e8
peterpadberg <peterpadberg@8b07a520-9580-ddfd-33fd-e530a354530b>
2011-01-11 11:33:04 +0000

    teruggezet

---
+++ trunk/KnockoutFase.java
@@ -0,0 +1,24 @@
+package Model;
+
+
+/**
+ * Write a description of class KnockoutFase here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class KnockoutFase{
+    private Knockout knockout;
+    
+    public KnockoutFase(){
+        this.nieuweKnockout();
+    }
+    
+    public void nieuweKnockout(){
+        knockout = new Knockout();
+    }
+    
+    public Knockout getKnockout(){
+        return knockout;
+    }
+}
---
+++ trunk/ToernooiLaden.java
@@ -0,0 +1,32 @@
+package Model;
+
+
+/**
+ * Write a description of class ToernooiLaden here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ToernooiLaden
+{
+    public static void main( String[] args) {
+        
+        Team team1 = new Team();
+        
+        String filenaam = "test.ser";
+        try {
+            ObjectInputStream uit = new ObjectInputStream( new FileInputStream(filenaam));
+            team = (Team)in.readObject();
+            uit.close();
+        }
+        
+        catch( IOException e ) {
+                    System.out.println( "Fout bij het openen, maken of sluiten van het bestand." );
+                    e.printStackTrace();
+        } 
+        
+        catch( ClassNotFoundException e ){
+               e.printStackTrace();
+        }
+    }
+}
